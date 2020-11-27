0bf6b79542a85f733f7503ae694be7db2a1e1037
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-11-27 13:09:37 -0800

    fixed typo in app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded());
+app.use(morgan("dev"));
 
 app.get("/", (request ,response) => {
     return response.status(200).json("Welcome to League-Spx");
 });
+
+
 
 app.listen(PORT, (request, response) => {
     console.log(`Server is listening on Port: ${PORT}`);
